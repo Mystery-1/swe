af728924a3e43a1c79d7aa7a3bc59f96ba2fee9d
Mystery-1 <146720434+Mystery-1@users.noreply.github.com>
2024-11-14 20:19:16 +0300

    Update getUserEmail.php

--- src/back/getUserEmail.php
+++ src/back/getUserEmail.php
@@ -1,6 +1,6 @@
 <?php
-header("Access-Control-Allow-Origin: http://localhost:3000"); // Adjust as needed
-header("Access-Control-Allow-Credentials: true"); // Allow credentials
+header("Access-Control-Allow-Origin: http://localhost:3000"); 
+header("Access-Control-Allow-Credentials: true"); 
 header("Access-Control-Allow-Methods: GET, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
 
